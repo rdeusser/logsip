e27fd4f6e2c6314864daf6676c14f26d6eeba010
iamthemuffinman <robertjdeusser@gmail.com>
2015-04-18 10:26:18 -0400

    Forgot to change logger to accept an io.Writer interface instead of just os.Stdout

--- logger.go
+++ logger.go
@@ -25,46 +25,46 @@ var red = color.New(color.FgRed).SprintFunc()
 
 func (l *Logger) Info(v ...interface{}) {
 	prefix := cyan("==> Info: ")
-	logger := log.New(os.Stdout, prefix, 0)
+	logger := log.New(l.out, prefix, 0)
 	logger.Println(v...)
 }
 func (l *Logger) Warn(v ...interface{}) {
 	prefix := yellow("==> Warn: ")
-	logger := log.New(os.Stdout, prefix, 0)
+	logger := log.New(l.out, prefix, 0)
 	logger.Println(v...)
 }
 func (l *Logger) Fatal(v ...interface{}) {
 	prefix := red("==> Fatal: ")
-	logger := log.New(os.Stdout, prefix+" -- ", log.Llongfile)
+	logger := log.New(l.out, prefix+" -- ", log.Llongfile)
 	logger.Println(v...)
 	os.Exit(1)
 }
 func (l *Logger) Panic(v ...interface{}) {
 	prefix := red("==> Panic: ")
-	logger := log.New(os.Stdout, prefix+" -- ", log.Llongfile)
+	logger := log.New(l.out, prefix+" -- ", log.Llongfile)
 	logger.Println(v...)
 	panic(logger)
 }
 
 func (l *Logger) Infof(format string, v ...interface{}) {
 	prefix := cyan("==> Info: ")
-	logger := log.New(os.Stdout, prefix, 0)
+	logger := log.New(l.out, prefix, 0)
 	logger.Printf(format, v...)
 }
 func (l *Logger) Warnf(format string, v ...interface{}) {
 	prefix := yellow("==> Warn: ")
-	logger := log.New(os.Stdout, prefix, 0)
+	logger := log.New(l.out, prefix, 0)
 	logger.Printf(format, v...)
 }
 func (l *Logger) Fatalf(format string, v ...interface{}) {
 	prefix := red("==> Fatal: ")
-	logger := log.New(os.Stdout, prefix+" -- ", log.Llongfile)
+	logger := log.New(l.out, prefix+" -- ", log.Llongfile)
 	logger.Printf(format, v...)
 	os.Exit(1)
 }
 func (l *Logger) Panicf(format string, v ...interface{}) {
 	prefix := red("==> Panic: ")
-	logger := log.New(os.Stdout, prefix+" -- ", log.Llongfile)
+	logger := log.New(l.out, prefix+" -- ", log.Llongfile)
 	logger.Printf(format, v...)
 	panic(logger)
 }
