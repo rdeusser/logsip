5f4fc66cf484dec4948f0f61bd246f2e65fbb912
iamthemuffinman <robertjdeusser@gmail.com>
2016-07-10 14:58:24 -0400

    Update Godocs

--- logsip.go
+++ logsip.go
@@ -47,83 +47,83 @@ func Default() *Logger {
 	}
 }
 
-// Info works just like log.Print, but with a Green "==> Info:" prefix.
+// Info works just like log.Print, but with a Green "==> INFO:" prefix.
 func (l *Logger) Info(v ...interface{}) {
 	l.Logger.Print(l.InfoPrefix + fmt.Sprint(v...))
 }
 
-// Debug works just like log.Print, but with a Cyan "==> Debug:" prefix.
+// Debug works just like log.Print, but with a Cyan "==> DEBUG:" prefix.
 func (l *Logger) Debug(v ...interface{}) {
 	if l.DebugMode {
 		l.Logger.Print(l.DebugPrefix + fmt.Sprint(v...))
 	}
 }
 
-// Warn works just like log.Print, but with a Yellow "==> Warn:" prefix.
+// Warn works just like log.Print, but with a Yellow "==> WARN:" prefix.
 func (l *Logger) Warn(v ...interface{}) {
 	l.Logger.Print(l.WarnPrefix + fmt.Sprint(v...))
 }
 
-// Fatal works just like log.Fatal, but with a Red "==> Fatal:" prefix.
+// Fatal works just like log.Fatal, but with a Red "==> FATAL:" prefix.
 func (l *Logger) Fatal(v ...interface{}) {
 	l.Logger.Fatal(l.FatalPrefix + fmt.Sprint(v...))
 }
 
-// Panic works just like log.Panic, but with a Red "==> Panic:" prefix.
+// Panic works just like log.Panic, but with a Red "==> PANIC:" prefix.
 func (l *Logger) Panic(v ...interface{}) {
 	l.Logger.Panic(l.PanicPrefix + fmt.Sprint(v...))
 }
 
-// Infof works just like log.Printf, but with a Green "==> Info:" prefix.
+// Infof works just like log.Printf, but with a Green "==> INFO:" prefix.
 func (l *Logger) Infof(format string, v ...interface{}) {
 	l.Logger.Print(l.InfoPrefix + fmt.Sprintf(format, v...))
 }
 
-// Debugf works just like log.Printf, but with a Cyan "==> Debug:" prefix.
+// Debugf works just like log.Printf, but with a Cyan "==> DEBUG:" prefix.
 func (l *Logger) Debugf(format string, v ...interface{}) {
 	if l.DebugMode {
 		l.Logger.Print(l.DebugPrefix + fmt.Sprintf(format, v...))
 	}
 }
 
-// Warnf works just like log.Printf, but with a Yellow "==> Warn:" prefix.
+// Warnf works just like log.Printf, but with a Yellow "==> WARN:" prefix.
 func (l *Logger) Warnf(format string, v ...interface{}) {
 	l.Logger.Print(l.WarnPrefix + fmt.Sprintf(format, v...))
 }
 
-// Fatalf works just like log.Fatalf, but with a Red "==> Fatal:" prefix.
+// Fatalf works just like log.Fatalf, but with a Red "==> FATAL:" prefix.
 func (l *Logger) Fatalf(format string, v ...interface{}) {
 	l.Logger.Fatalf(l.FatalPrefix + fmt.Sprintf(format, v...))
 }
 
-// Panicf works just like log.Panicf, but with a Red "==> Panic:" prefix.
+// Panicf works just like log.Panicf, but with a Red "==> PANIC:" prefix.
 func (l *Logger) Panicf(format string, v ...interface{}) {
 	l.Logger.Panicf(l.PanicPrefix + fmt.Sprintf(format, v...))
 }
 
-// Infoln works just like log.Println, but with a Green "==> Info:" prefix.
+// Infoln works just like log.Println, but with a Green "==> INFO:" prefix.
 func (l *Logger) Infoln(v ...interface{}) {
 	l.Logger.Println(l.InfoPrefix + fmt.Sprint(v...))
 }
 
-// Debugln works just like log.Println, but with a Cyan "==> Debug:" prefix.
+// Debugln works just like log.Println, but with a Cyan "==> DEBUG:" prefix.
 func (l *Logger) Debugln(v ...interface{}) {
 	if l.DebugMode {
 		l.Logger.Println(l.DebugPrefix + fmt.Sprint(v...))
 	}
 }
 
-// Warnln works just like log.Println, but with a Yellow "==> Warn:" prefix.
+// Warnln works just like log.Println, but with a Yellow "==> WARN:" prefix.
 func (l *Logger) Warnln(v ...interface{}) {
 	l.Logger.Println(l.WarnPrefix + fmt.Sprint(v...))
 }
 
-// Fatalln works just like log.Fatalln, but with a Red "==> Fatal:" prefix.
+// Fatalln works just like log.Fatalln, but with a Red "==> FATAL:" prefix.
 func (l *Logger) Fatalln(v ...interface{}) {
 	l.Logger.Fatalln(l.FatalPrefix + fmt.Sprint(v...))
 }
 
-// Panicln works just like log.Panicln, but with a Red "==> Panic:" prefix.
+// Panicln works just like log.Panicln, but with a Red "==> PANIC:" prefix.
 func (l *Logger) Panicln(v ...interface{}) {
 	l.Logger.Panicln(l.PanicPrefix + fmt.Sprint(v...))
 }
