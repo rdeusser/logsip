1656384f2aeb663b53311028d740c92d333a615a
iamthemuffinman <robertjdeusser@gmail.com>
2016-09-26 17:20:48 -0400

    Add integration test

--- integration_test.go
+++ integration_test.go
@@ -1,29 +1,155 @@
-package logsip
+package logsip_test
 
 import (
+	"os"
+	"os/exec"
 	"testing"
+
+	log "github.com/iamthemuffinman/logsip"
 )
 
-func TestDebug(t *testing.T)   {}
-func TestDebugf(t *testing.T)  {}
-func TestDebugln(t *testing.T) {}
+const testString = "this is a test string"
 
-func TestInfo(t *testing.T)   {}
-func TestInfof(t *testing.T)  {}
-func TestInfoln(t *testing.T) {}
+func TestNew(t *testing.T) {
+	logger := log.New()
 
-func TestWarn(t *testing.T)   {}
-func TestWarnf(t *testing.T)  {}
-func TestWarnln(t *testing.T) {}
+	logger.Info(testString)
+}
 
-func TestError(t *testing.T)   {}
-func TestErrorf(t *testing.T)  {}
-func TestErrorln(t *testing.T) {}
+func TestDebug(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	log.Debug(testString)
+}
 
-func TestFatal(t *testing.T)   {}
-func TestFatalf(t *testing.T)  {}
-func TestFatalln(t *testing.T) {}
+func TestDebugf(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	log.Debugf("%s", testString)
+}
 
-func TestPanic(t *testing.T)   {}
-func TestPanicf(t *testing.T)  {}
-func TestPanicln(t *testing.T) {}
+func TestDebugln(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	log.Debugln(testString)
+}
+
+func TestInfo(t *testing.T) {
+	log.Info(testString)
+}
+
+func TestInfof(t *testing.T) {
+	log.Infof("%s", testString)
+}
+
+func TestInfoln(t *testing.T) {
+	log.Infoln(testString)
+}
+
+func TestWarn(t *testing.T) {
+	log.Warn(testString)
+}
+
+func TestWarnf(t *testing.T) {
+	log.Warnf("%s", testString)
+}
+
+func TestWarnln(t *testing.T) {
+	log.Warnln(testString)
+}
+
+func TestError(t *testing.T) {
+	log.Error(testString)
+}
+
+func TestErrorf(t *testing.T) {
+	log.Errorf("%s", testString)
+}
+
+func TestErrorln(t *testing.T) {
+	log.Errorln(testString)
+}
+
+func TestFatal(t *testing.T) {
+	if os.Getenv("BE_CRASHER") == "1" {
+		log.Fatal(testString)
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestFatal")
+	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
+}
+
+func TestFatalf(t *testing.T) {
+	if os.Getenv("BE_CRASHER") == "1" {
+		log.Fatal(testString)
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestFatalf")
+	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
+}
+
+func TestFatalln(t *testing.T) {
+	if os.Getenv("BE_CRASHER") == "1" {
+		log.Fatal(testString)
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestFatalln")
+	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
+}
+
+func TestPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("it didn't panic")
+		}
+	}()
+
+	log.Panic(testString)
+}
+
+func TestPanicf(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("it didn't panic")
+		}
+	}()
+
+	log.Panicf("%s", testString)
+}
+
+func TestPanicln(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("it didn't panic")
+		}
+	}()
+
+	log.Panicln(testString)
+}
+
+func TestLevel(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	log.Debug(testString)
+	l := log.GetLevel()
+	if l != log.DebugLevel {
+		t.Errorf("expected %s, got %s", log.DebugLevel, log.InfoLevel)
+	}
+	log.SetLevel(log.InfoLevel)
+	log.Debug(testString)
+	n := log.GetLevel()
+	if n != log.InfoLevel {
+		t.Errorf("expected %s, got %s", log.InfoLevel, log.DebugLevel)
+	}
+}
